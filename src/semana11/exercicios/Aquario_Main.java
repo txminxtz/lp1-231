05b720e3a99101090a72924ecb68a6646eaaf853
txminxtz <126599638+txminxtz@users.noreply.github.com>
2023-05-20 11:25:04 -0300

    Add files via upload

---
+++ src/semana11/exercicios/Aquario_Main.java
@@ -0,0 +1,61 @@
+package semana12.exercicios;
+
+import javax.swing.JOptionPane;
+
+public class Aquario_Main {
+    
+    public static void main(String[] args) {
+        
+        float 
+            fComprimento, 
+            fLargura, 
+            fAltura,
+            fVolume,
+            fPotencia,
+            fTemp_Ambiente,
+            fTemp_Desejada,
+            fFiltragem_Min,
+            fFiltragem_Max;
+
+        Aquario aquario1;
+
+        fComprimento = Float.parseFloat(JOptionPane.showInputDialog("Comprimento (cm): "));
+        fLargura = Float.parseFloat(JOptionPane.showInputDialog("Largura (cm): "));
+        fAltura = Float.parseFloat(JOptionPane.showInputDialog("Altura (cm): "));
+        fTemp_Ambiente = Float.parseFloat(JOptionPane.showInputDialog("Temperatura Ambiente (oC): "));
+        fTemp_Desejada = Float.parseFloat(JOptionPane.showInputDialog("Temperatura Desejada (oC): "));
+
+        aquario1 = new Aquario(fComprimento, fLargura, fAltura);            
+
+
+        fVolume = aquario1.f_Func_CalcularVolume();
+
+        fPotencia = aquario1.f_Func_CalcularPotenciaDoTermostato(
+            fVolume, 
+            fTemp_Desejada, 
+            fTemp_Ambiente 
+        );
+
+        fFiltragem_Min = aquario1.f_Func_CalculaQuantidadeLitrosFiltro(
+            fVolume, 
+            2
+        );
+
+        fFiltragem_Max = aquario1.f_Func_CalculaQuantidadeLitrosFiltro (
+            fVolume, 
+            3
+        );
+
+        System.out.println("Comprimento (cm): " + fComprimento);
+        System.out.println("Largura (cm): " + fLargura);
+        System.out.println("Altura (cm): " + fAltura);
+        System.out.println("Temperatura Ambiente (oC): " + fTemp_Ambiente);
+        System.out.println("Temperatura Desejada (oC): " + fTemp_Desejada);
+        System.out.println("Volume (l): " + fVolume);
+        System.out.println("Potencia (W): " + fPotencia);
+        System.out.println("Filtragem Mínima (l/h): " + fFiltragem_Min);
+        System.out.println("Filtragem Máxima (l/h): " + fFiltragem_Max);
+         
+     }
+  
+}
