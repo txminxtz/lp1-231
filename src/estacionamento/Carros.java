e269cd2dab961c52df96f1a825aebaf281e84b26
txminxtz <126599638+txminxtz@users.noreply.github.com>
2023-09-19 11:09:08 -0300

    Add files via upload

---
+++ src/estacionamento/Carros.java
@@ -0,0 +1,79 @@
+package estacionamento;
+
+import java.sql.*;
+
+class Carros{
+
+    private String placa;
+    private String montadora;
+    private double modelo;
+    private double cor;
+    private double mensalidade;
+
+    public static void main(String args[]){
+
+        String sPlaca, sMontadora, sModelo, sCor, sMensalidade, sComando;
+        ResultSet rs;
+
+        try
+        {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_estacionamento","root","bts4");
+
+            Statement stmt=con.createStatement();
+
+            System.out.println();
+            System.out.println("INCLUSAO DE CARROS");
+            System.out.println();
+
+            sPlaca = "HJU6783";
+            sMontadora = "honda";
+            sModelo = "hrv";
+            sCor = "prata";
+            sMensalidade = "100";
+            
+            sComando = "INSERT INTO tbl_carros " +
+                        "(placa, montadora, modelo, cor, mensalidade) " +
+                        "VALUE (null, " +
+                        "'" + sPlaca + "'" + ", " +
+                        "'" + sMontadora + "'"+ ", " +
+                        "'" + sModelo + "'" + ", " +
+                        "'" + sCor + "'" + ", " +
+                        "'" + sMensalidade + "'" + 
+                        ")";
+
+            stmt.execute(sComando);
+
+
+            System.out.println();
+            System.out.println("RELATORIO DE CARROS");
+            System.out.println();
+
+            rs = stmt.executeQuery("select * from tbl_carros");
+
+            while(rs.next()){
+                System.out.println(
+                    rs.getInt(1)    +"  "+
+                    rs.getString(2) +"  "+
+                    rs.getString(3) +"  "+
+                    rs.getString(3) +"  "+
+                    rs.getString(4) +"  "+
+                    rs.getString(5)
+                    );
+            }           
+
+            System.out.println();
+
+            con.close();
+
+        }
+        catch(Exception e)
+        { 
+            System.out.println(e);
+        }
+
+    }
+
+}
