6d32c2cb6af1b4bd5309ec1fd22ea6fdd667a8c3
Taminato <126599638+txminxtz@users.noreply.github.com>
2023-10-14 15:40:45 -0300

    Add files via upload

--- src/estacionamento/ConnectionFactory.java
+++ src/estacionamento/ConnectionFactory.java
@@ -6,6 +6,7 @@ public class ConnectionFactory {
 
     String sJdbc = "com.mysql.cj.jdbc.Driver";
     String url = "jdbc:mysql://localhost:3306/db_estacionamento";
+    //String url = "jdbc:mysql://localhost/estudante?user=estudante&password=estudante&useSSl=true";
     String user = "root";
     String password = "bts4";
 
@@ -14,8 +15,11 @@ public class ConnectionFactory {
     public Connection createConnection() throws SQLException
     {
         try {
+        
             Class.forName(sJdbc);
             return DriverManager.getConnection(url, user, password);
+            //return DriverManager.getConnection(url);
+
         }
         catch (ClassNotFoundException e) {
             System.err.println("Conexao nao Estabelecida!");
